9876cda18eed82fc990df81950e0aefce34ddf0e
BisousBaveux <85183715+BisousBaveux@users.noreply.github.com>
2021-09-15 17:35:39 +0200

    epfv1
    script javascript de l'épreuve du feu

---
+++ Majuscule.js
@@ -0,0 +1,21 @@
+var phrase = process.argv[2];
+majuscule(phrase)
+var phraseModifiee="";
+
+function majuscule(phrase){
+    for (i=0; i< phrase.length; i++){
+        if(i==0){
+            phraseModifiee = phrase.charAt(i).toLowerCase();
+        } else {
+            if(phraseModifiee[phraseModifiee.length-1] == phraseModifiee.charAt(phraseModifiee.length-1).toLowerCase()){ //length -1 car on enlève la diff entre l'index et le nb de caractère
+                phraseModifiee = phraseModifiee + phrase.charAt(i).toUpperCase(); //A chaque lettre modifiée, PhrModif se transforme, et une lettre par une lettre, finit par nous donner la phrase entière
+            } else if (phrase.charAt(i) == " "){ // On prend en compte les espaces
+                phraseModifiee = phraseModifiee + phrase.charAt(i)
+            } else {
+                phraseModifiee = phraseModifiee + phrase.charAt(i).toLowerCase();
+            }
+        }
+    }
+    console.log(phraseModifiee)
+
+    }
